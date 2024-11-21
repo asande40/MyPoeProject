e2a2c8360637984d39c6c5105db78ca9aa827264
RC_Student_lab <RC_Student_lab@10.0.0.70>
2024-11-21 07:37:14 +0000

    did my first tests of the methods

---
+++ src/test/java/com/mycompany/mypoe/part3Test.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.mypoe;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class part3Test {
+    
+   
+ 
+
+    /**
+     * Test of showInvalidOptionMessage method, of class part3.
+     */
+
+
+    /**
+     * Test of addTask method, of class part3.
+     */
+    @org.junit.Test
+    public void testAddTask() {
+        System.out.println("addTask");
+        part3 instance = new part3();
+        instance.addTask();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+   
+    }
+
+ 
